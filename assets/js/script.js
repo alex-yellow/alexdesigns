412f170b84fa3168d170a693ffe6d2112c8c6f62
alex-yellow <72997598+alex-yellow@users.noreply.github.com>
2020-12-28 16:35:20 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("form").submit(function(){var th=$(this);$.ajax({type:"POST",url:"mail.php",data:th.serialize()}).done(function(){swal("Отлично!","Ваше сообщение отправлено!","success");setTimeout(function(){th.trigger("reset");},1000);});return false;});});
